c06ce5048089d5917182ea9557ef7d6e9f4ee027
FoyerD <amitfoye@post.bgu.ac.il>
2024-02-23 16:19:09 +0200

    AI no longer has delay

--- src/main/java/bguspl/set/ex/Player.java
+++ src/main/java/bguspl/set/ex/Player.java
@@ -157,12 +157,6 @@ public class Player implements Runnable {
                 synchronized(this.keyQueue){
                     while (this.keyQueue.size() < 3 && !terminate){
                         keyPressed(rnd.nextInt(this.env.config.tableSize));
-
-                        //note: sleeping while holding the key is a bit wasefull,
-                        //but the only one thread that waits for the queue to be empty is the player thread
-                        //so it wont cause a thread to wait for nothing
-                        try{Thread.sleep(rnd.nextInt(500) + 500);} //This is A smart ai not a fast ai
-                        catch(InterruptedException e){}
                     }
                 }
                 try{
